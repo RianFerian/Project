d2c67ceb5e752b3724d482e16c84298678706dd5
Rian Ferian <rianferian46@gmail.com>
2023-10-07 18:20:38 +0700

    QR Code reader finished project

--- QR code/QR reader2.py
+++ QR code/QR reader2.py
@@ -1,5 +1,4 @@
 import cv2
-# import numpy as np
 from pyzbar.pyzbar import decode
 import webbrowser
 import time
@@ -7,16 +6,10 @@ import time
 # Take the video
 cap = cv2.VideoCapture(0)
 
-
 while True:
 
     sucess, img = cap.read()
     for barcode in decode(img):
-
-        # Create Polylines
-        # pts = np.array([barcode.polygon], np.int32)
-        # pts = pts.reshape((-1,1,2))
-        # cv2.polylines(img, [pts], True, (255,0,255), 5)
 
         # Decode URL
         myData = barcode.data.decode('utf-8')
@@ -25,7 +18,7 @@ while True:
         webbrowser.open_new(myData)
 
         # # Time for executing
-        time.sleep(10)
+        time.sleep(3)
         
     cv2.imshow('Result', img)
     cv2.waitKey(1)
