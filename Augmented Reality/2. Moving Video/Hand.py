9665dc5880918c6126306e127956ab58ee9e3eeb
Rian Ferian <rianferian46@gmail.com>
2023-10-16 22:50:19 +0700

    adding FPS

--- Augmented Reality/2. Moving Video/Hand.py
+++ Augmented Reality/2. Moving Video/Hand.py
@@ -15,6 +15,10 @@ while True:
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
+    pTime = cTime
 
     cv2.putText(img,f'FPS:{int(fps)}', (40, 58), cv2.FONT_HERSHEY_COMPLEX,
                 1, (255, 0, 0), 3)
+    
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
