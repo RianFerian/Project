f87f762e9fde47429fc18d11fca39ee39cc92b52
Rian Ferian <rianferian46@gmail.com>
2023-12-17 14:46:51 +0700

    Frontend connected to backend

--- Web Admin 2/App.py
+++ Web Admin 2/App.py
@@ -1,6 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, g
+import sqlite3
+import os
 
 app = Flask(__name__)
+
+# Get the directory containing the currently executing Python script
+folder_path = os.path.dirname(os.path.abspath(__file__))
+
+# Connect to the database file inside the specified folder
+db_path = os.path.join(folder_path, 'mydatabase.db')
+DEBUG = True
+
+# Function to connect to the database
+def connect_db():
+    return sqlite3.connect(db_path)
+
+# Get the database connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+# Close the database connection when the request is finished
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def Index():
@@ -10,7 +35,22 @@ def Index():
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/insert', methods = ['POST'])
+def insert():
+    print("Reached the /insert route")
 
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        print(f"Received form data: {name}, {email}, {phone}")
+
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO employee (name, email, phone) VALUES (?, ?, ?)", (name, email, phone))
+        db.commit()
+
+        return redirect(url_for('dashboard'))
 
 
 
--- Web Admin 2/view.py
+++ Web Admin 2/view.py
@@ -1,27 +1,29 @@
 import sqlite3
 import os
 
+
 # Get the directory containing the currently executing Python script
 folder_path = os.path.dirname(os.path.abspath(__file__))
 
 # Connect to the database file inside the specified folder
 db_path = os.path.join(folder_path, 'mydatabase.db')
 
+# Connect to the SQLite database
 connection = sqlite3.connect(db_path)
 
+# Create a cursor object to execute SQL queries
 cursor = connection.cursor()
 
-# Example: Creating a 'users' table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS employee (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL,
-        phone TEXT NOT NULL
-    )
-''')
+# Execute a SELECT query to fetch all rows from the 'employee' table
+cursor.execute("SELECT * FROM employee")
 
-# Commit the changes
-connection.commit()
+# Fetch all rows from the result set
+data = cursor.fetchall()
 
+# Print the data
+for row in data:
+    print(row)
+
+# Close the cursor and connection
+cursor.close()
 connection.close()
